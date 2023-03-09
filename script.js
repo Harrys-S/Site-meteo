68267bf66a1aded672a817d7de23ff3fa45cd3ad
Harrys SOTTIN <87320355+Harrys-S@users.noreply.github.com>
2023-03-09 23:42:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-let calleSuccess = function(data) {
-	console.log("donnees meteo", data)
-	let element = document.getElementById("result");
-	let element2 = document.getElementById("result2");
-	let element3 = document.getElementById("result3");
-	element.innerHTML = "La température est de: "+ data.main.temp + "°C";
-	element2.innerHTML = "Dominant: "+ data.weather[0].main;
-	element3.innerHTML = "Description: "+ data.weather[0].description;
-}
-
 function buttonClick() {
 	let url = "https://api.openweathermap.org/data/2.5/weather?q=";
 	let key = "&lang=fr&units=metric&appid=3f0792fc888059276dc12063b484af4d";
 	let ville = document.getElementById("city").value;
 	url += ville + key;
-	$.get(url, calleSuccess).fail(function() {
-		alert("error");
-	})
-	let body = document.queryselector("body");
-	let x = data.weather[0].main;
-	let y = "background-image: url(\'" + x + ".jpg\')";
-	alert("Hello");
-	body.setAttribute("style", y);
+    fetch(url)
+        .then(response => response.json())
+        .then(function callSuccess(data){
+            console.log(data);
+            let element = document.getElementById("result");
+            let element2 = document.getElementById("result2");
+            let element3 = document.getElementById("result3");
+            element.innerHTML = "La température est de: "+ data.main.temp + "°C";
+            element2.innerHTML = "Dominant: "+ data.weather[0].main;
+            element3.innerHTML = "Description: "+ data.weather[0].description;
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
 }
